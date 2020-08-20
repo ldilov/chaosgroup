36efab535b5a08f14c524c5cbbf51ec770a461a7
Lazar Dilov <lsfcultras@gmail.com>
2020-08-20 16:05:28 +0300

    Added new suite of scenes

---
+++ third_scene.test.json.js
@@ -0,0 +1,11 @@
+[
+    {
+        "scene": "Third  scene",
+        "success": true,
+        "time": 22
+    }, {
+    "scene": "Third other dummy scene",
+    "success": false,
+    "time": 8
+}
+]
